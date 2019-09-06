424ad212a91d3ac3fb3e7556c8cfa48bfd674d53
andre <andregomars@gmail.com>
2019-09-06 14:53:45 -0700

    add home and about pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', loadChildren: './views/home/home.module#HomeModule' },
+  { path: 'about', loadChildren: './views/about/about.module#AboutModule' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
