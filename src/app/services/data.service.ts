74d7d1d1135f8b57146f31e4032ab220e6de6498
andre <andregomars@gmail.com>
2020-02-06 13:10:25 -0800

    add cdk virtual scroll with dynamic rows

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,67 +2,84 @@ import { Injectable } from '@angular/core';
 import { Team } from '../models/team';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class DataService {
-   getTeams(): Team[] {
-       return [
-        {
-            name: "Patriots",
-            logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/NE.svg"
-        },
-        {
-            name: "Bills",
-            logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/BUF.svg"
-        },
-        {
-            name: "Jets",
-            logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/NYJ.svg"
-        },
-        {
-            name: "Dolphins",
-            logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/MIA.svg"
-        },
-        {
-            name: "Ravens",
-            logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/BAL.svg"
-        },
-        {
-            name: "Steelers",
-            logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/PIT.svg"
-        },
-        {
-            name: "Browns",
-            logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/CLE.svg"
-        },
-        {
-            name: "Bengals",
-            logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/CIN.svg"
-        },
-        {
-            name: "Texans",
-            logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/HOU.svg"
-        },
-        {
-            name: "Titans",
-            logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/TEN.svg"
-        },
-        {
-            name: "Colts",
-            logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/IND.svg"
-        },
-        {
-            name: "Jaguars",
-            logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/JAX.svg"
-        },
-        {
-            name: "Chiefs",
-            logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/KC.svg"
-        },
-        {
-            name: "Broncos",
-            logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/DEN.svg"
-        },
-    ];
-   }
+    getTeams(): Team[] {
+        return [
+            {
+                name: "Patriots",
+                logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/NE.svg"
+            },
+            {
+                name: "Bills",
+                logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/BUF.svg"
+            },
+            {
+                name: "Jets",
+                logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/NYJ.svg"
+            },
+            {
+                name: "Dolphins",
+                logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/MIA.svg"
+            },
+            {
+                name: "Ravens",
+                logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/BAL.svg"
+            },
+            {
+                name: "Steelers",
+                logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/PIT.svg"
+            },
+            {
+                name: "Browns",
+                logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/CLE.svg"
+            },
+            {
+                name: "Bengals",
+                logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/CIN.svg"
+            },
+            {
+                name: "Texans",
+                logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/HOU.svg"
+            },
+            {
+                name: "Titans",
+                logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/TEN.svg"
+            },
+            {
+                name: "Colts",
+                logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/IND.svg"
+            },
+            {
+                name: "Jaguars",
+                logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/JAX.svg"
+            },
+            {
+                name: "Chiefs",
+                logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/KC.svg"
+            },
+            {
+                name: "Broncos",
+                logo: "https://static.nfl.com/static/content/public/static/wildcat/assets/img/logos/teams/DEN.svg"
+            },
+        ];
+    }
+
+    getScrollableList<T>(list: Array<T>, size: number): Array<T>[] {
+        const chunks = [];
+        while (list.length > 0 && size > 0) {
+            chunks.push(list.splice(0, size));
+        }
+        return chunks;
+    }
+
+    mobile(): boolean {
+        const userAgent = navigator.userAgent;
+        return /\b(iPhone|iP[ao]d)/.test(userAgent) ||
+            /\b(iP[ao]d)/.test(userAgent) ||
+            /Android/i.test(userAgent) ||
+            /Mobile/i.test(userAgent) ||
+            window.innerWidth <= 768;
+    }
 }
--- src/app/views/virtualscroll/virtualscroll.component.ts
+++ src/app/views/virtualscroll/virtualscroll.component.ts
@@ -8,12 +8,16 @@ import { DataService } from '../../services/data.service';
   styleUrls: ['./virtualscroll.component.scss']
 })
 export class VirtualScrollComponent implements OnInit {
-  items: Team[];
+  teams: Team[];
+  teamRows: Array<Team>[];
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-    this.items = this.dataService.getTeams();
+    this.teams = this.dataService.getTeams();
+    const list = this.dataService.getTeams();
+    const size = this.dataService.mobile() ? 1 : 4;
+    this.teamRows = this.dataService.getScrollableList<Team>(list, size);
   }
 
 }
