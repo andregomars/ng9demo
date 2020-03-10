50160c66dac3e5cfbdaf25fa394a101a14f4185f
andregomars <andregomars@gmail.com>
2020-03-09 23:58:19 -0700

    add meta copyright

--- src/app/services/utility.service.ts
+++ src/app/services/utility.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { LinkDefinition, LinkService } from './link.service';
 import { MetaDefinition, Meta, Title } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root',
@@ -10,10 +13,18 @@ export class UtilityService {
     constructor(private meta: Meta,
         private title: Title,
         private link: LinkService,
+        private http: HttpClient
     ) { }
 
     setHeaderTagsByPath() {
         this.setHeaderTags(location.pathname.replace('/', ''));
+    }
+
+    private test() {
+        const url = 'http://www.mocky.io/v2/5e6738d13100007b0023071d';
+        return this.http.get<{name: string}>(url).subscribe(
+            x => { console.log(x); }
+        );
     }
 
     setHeaderTags(keyword: string) {
@@ -21,7 +32,10 @@ export class UtilityService {
         this.buildMetas(keyword).map(meta => {
             this.meta.updateTag(meta)
         });
-        // this.link.addTag(this.buildCanonical(keyword));
+        this.buildCopyRight().subscribe(copyright => 
+            this.meta.updateTag(copyright));
+        // this.buildIpMeta().subscribe(ipAddress => 
+        //     this.meta.updateTag(ipAddress));
         this.link.updateTag(this.buildCanonical(keyword));
     }
 
@@ -49,5 +63,27 @@ export class UtilityService {
             }
         ]
     }
+
+    private buildIpMeta(): Observable<MetaDefinition> {
+        const url = 'http://api.ipify.org?format=json';
+        return this.http.get<{ip: string}>(url).pipe(
+            map(data => { return {
+                name: 'customerIP',
+                content: data.ip
+            } })
+        );
+    }
+
+    private buildCopyRight(): Observable<MetaDefinition> {
+        const url = 'http://www.mocky.io/v2/5e673944310000a700230721';
+        return this.http.get<{copyright: string}>(url).pipe(
+            map(data => { 
+                return {
+                name: 'copyright',
+                content: data.copyright
+            } })
+        );
+    }
+    
 
 }
