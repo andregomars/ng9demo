fe587310181174701d8fce365b96c7b5234349dc
andregomars <andregomars@gmail.com>
2020-03-14 22:46:30 -0700

    alter ng8demo to ng9demo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng8demo'`, () => {
+  it(`should have as title 'ng9demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng8demo');
+    expect(app.title).toEqual('ng9demo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng8demo app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ng9demo app is running!');
   });
 });
